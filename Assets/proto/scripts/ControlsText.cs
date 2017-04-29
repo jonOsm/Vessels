039788846273b6d159227f07b98b46b91036beba
Jonathan Osmond <jonathancosmond@gmail.com>
2017-04-29 15:31:40 -0400

    add controls panel

---
+++ Assets/proto/scripts/ControlsText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsText : MonoBehaviour {
+
+	public float timeToDisplay = 15;	
+	// Use this for initialization
+	void Start () {
+		Invoke("Die", timeToDisplay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Cancel")) {
+			Die();
+		};
+	}
+
+	void Die() {
+		Destroy(gameObject);	
+	}
+}
