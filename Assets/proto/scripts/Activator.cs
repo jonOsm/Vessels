42d1758d48f7fc1d7a36c48407a187eb57e960c3
Jonathan Osmond <jonathancosmond@gmail.com>
2017-04-29 10:02:02 -0400

    add lonely island, create activator and IActivatable scripts

---
+++ Assets/proto/scripts/Activator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour {
+	public enum ActivatorType {
+		SINGLEUSE,
+		TOGGLE,
+		MULTIUSE
+	}
+	public GameObject activatableGameObject;
+	public ActivatorType activatorType = ActivatorType.SINGLEUSE;
+	public bool enabled = true;
+
+	private IActivatable activatable;
+	private int timesActivated = 0;
+
+	// Use this for initialization
+	void Start () {
+		 activatable = activatableGameObject.GetComponent<IActivatable>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (activatorType == ActivatorType.SINGLEUSE && timesActivated > 0)	{
+			Disable();
+		}
+
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (enabled) {
+			activatable.Activate();
+			timesActivated++;
+		}
+
+	}
+	
+	void Disable() {
+		enabled = false;
+	}
+
+}
---
+++ Assets/proto/scripts/Platform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour, IActivatable{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Activate() {
+		Debug.Log("activated");
+	}
+}
