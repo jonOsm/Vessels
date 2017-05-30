b155e8923f2e21c9134a091799a55b220c8bd5f1
Jonathan Osmond <j.osmond@live.com>
2017-05-30 19:47:20 -0400

    add simple door locking, place doors

--- Assets/DoorBehavior.cs
+++ Assets/DoorBehavior.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorBehavior : MonoBehaviour {
+public class DoorBehavior : MonoBehaviour, IActivatable {
 
 	public bool playerNear = false;	
+	public bool isLocked = false;
 	Animator animator;
 	// Use this for initialization
 	void Start () {
@@ -18,6 +19,7 @@ public class DoorBehavior : MonoBehaviour {
 
 	void OnTriggerEnter(Collider col) {
 		if (col.gameObject.GetComponent<Interactor>()) {
+			if (isLocked) return;
 			playerNear = true;
 			animator.SetBool("playerNear", true);	
 		}
@@ -25,8 +27,22 @@ public class DoorBehavior : MonoBehaviour {
 
 	void OnTriggerExit(Collider col) {
 		if (col.gameObject.GetComponent<Interactor>()) {
+			if (isLocked) return;
 			playerNear = true;
 			animator.SetBool("playerNear", false);	
 		}
+	}
+
+	void Unlock() {
+		//do something
+		isLocked = false;
+	}
+
+	public void Activate()  {
+		Unlock();
+	}
+
+	public void Deactivate()  {
+
 	}
 }
